454ce9792d615e63f08e15935229321e52204940
JonRojas <jonathan.rojas@gmail.com>
2015-05-20 11:26:28 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+Tabletop.init({ 
+  key: "https://docs.google.com/spreadsheets/u/1/d/1_bU0idTkrU4c0P3Y4ha3KeULIQ8fL22t8mffOETafTA/pubhtml",
+  callback: sheetLoad,
+  simpleSheet: true 
+})
+
+function sheetLoad( data, tabletop ){
+  render( data )
+}
+
+function render( context ){
+  var template = $("#employee-template").html()
+  var compile = Handlebars.compile( template )
+  var html = compile( { employees: context } )
+  $(".js-profiles").html( html ).show()
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+# GA DC Staff!
